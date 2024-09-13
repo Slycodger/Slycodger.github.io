d0053521403358ccd1a9e059b0d2b20aa3d83a93
Slycodger <slycodger@gmail.com>
2024-09-12 23:07:49 -0500

    fix

--- BasicApp/Run.js
+++ BasicApp/Run.js
@@ -1,5 +1,3 @@
-var ParentBox;
-
 startBackground(...[new pageButton("home", "../index.html"), new pageButton("about", "../About/index.html")]);
 br();
 
@@ -7,4 +5,4 @@ addText("descriptionText", "BasicApp", "h1");
 br();
 addDescription("No git yet so no download either :(");
 br();
-addText("bigText", "This is going to be either a simple 2D app maker since I have some things I want to make, or a painting app.  Haven't decided yet.", "h1"
+addText("bigText", "This is going to be either a simple 2D app maker since I have some things I want to make, or a painting app.  Haven't decided yet.", "h1");
